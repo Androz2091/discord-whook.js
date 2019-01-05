ac7436366bcabb78f53e166b13909ad7f64f04f1
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-01-05 23:06:29 +0100

    Update EmbedBuilder.js

--- src/EmbedBuilder.js
+++ src/EmbedBuilder.js
@@ -25,8 +25,8 @@ class EmbedBuilder {
     setTitle(title){
         if(!title) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a title to set a title!");
         if(title.length > 256) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a title with 256 characters max.!");
-        this.embed[0].title = title;
-        return this;
+            this.embed[0].title = title;
+            return this;
     }
  
     /**
@@ -37,8 +37,8 @@ class EmbedBuilder {
     setDescription(description) {
         if(!description) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a description to set a description!");
         if(description.length > 2048) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a description with 2048 characters max.!");
-        this.embed[0].description = description;
-        return this;
+            this.embed[0].description = description;
+            return this;
     }
 
     /**
@@ -48,10 +48,10 @@ class EmbedBuilder {
      */
     setImage(imageURL) {
         if(!imageURL) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a imageURL to set an image!");
-        this.embed[0].image = {
-            url: imageURL
-        };
-        return this;
+            this.embed[0].image = {
+                url: imageURL
+            };
+            return this;
     }
 
     /**
@@ -61,10 +61,10 @@ class EmbedBuilder {
      */
     setThumbnail(imageURL) {
         if(!imageURL) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a text to set a footer!");
-        this.embed[0].thumbnail = {
-            url: imageURL
-        };
-        return this;
+            this.embed[0].thumbnail = {
+                url: imageURL
+            };
+            return this;
     }
 
     /**
@@ -77,11 +77,11 @@ class EmbedBuilder {
         if(!text) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a text to set a footer!");
         if(text.length > 2048) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a text of a footer with 2048 characters max.!");
         if(!iconURL) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a iconURL to set a footer!");
-        this.embed[0].footer = {
-            text: text,
-            icon_url: iconURL
-        };
-        return this;
+            this.embed[0].footer = {
+                text: text,
+                icon_url: iconURL
+            };
+            return this;
     }
 
     /**
@@ -94,11 +94,11 @@ class EmbedBuilder {
         if(!name) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a name to set an author!");
         if(name.length > 256) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide an author with 256 characters max.!");
         if(!iconURL) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a iconURL to set a author!");
-        this.embed[0].author = {
-            name: name,
-            icon_url: iconURL
-        };
-        return this;
+            this.embed[0].author = {
+                name: name,
+                icon_url: iconURL
+            };
+            return this;
     }
 
     /**
@@ -108,8 +108,8 @@ class EmbedBuilder {
      */
     setColor(color) {
         if(!color) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a color to set a color!");
-        this.embed[0].color = color;
-        return this;
+            this.embed[0].color = color;
+            return this;
     }
 
     /**
@@ -125,12 +125,12 @@ class EmbedBuilder {
         if(!value) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a value to add a field!");
         if(value.length > 1024) throw new Error("[DiscordWebhook-EmbedBuilder] You must provide a value of a field with 1024 characters max.!");
         if(this.embed[0].fields.length > 25) throw new Error("[DiscordWebhook-EmbedBuilder] You can include 25 fields max.!");
-        this.embed[0].fields.push({
-            name,
-            value,
-            inline: inline ? true : false
-        });
-        return this;
+            this.embed[0].fields.push({
+                name,
+                value,
+                inline: inline ? true : false
+            });
+            return this;
     }
 };
 
